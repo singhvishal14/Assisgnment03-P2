53672a2c01f87f5f20b27e8854bc2e27f8d03fbb
singhvishal14 <kumarvishalsingh767@gmail.com>
2023-07-14 13:50:00 +0530

    Assisgnment3 P2

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisgnment03_P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Convert string to integer using TryParse
+            string intStr = "30";
+            int convertedInt;
+            if (int.TryParse(intStr, out convertedInt))
+            {
+                int multipliedValue = convertedInt * 5;
+                Console.WriteLine("Final value: " + multipliedValue);
+            }
+            else
+            {
+                Console.WriteLine("Error occured : Invalid integer format");
+            }
+
+            // Convert string to DateTime using Convert
+            string dateTimeStr = "2023-05-29";
+            try
+            {
+                DateTime convertedDateTime = Convert.ToDateTime(dateTimeStr);
+                DateTime subtractedDateTime = convertedDateTime.AddMonths(-1);
+                Console.WriteLine("Subtracted DateTime: " + subtractedDateTime);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error occured: Invalid DateTime format");
+            }
+
+            // Convert string to TimeSpan using Parse
+            string timeSpanStr = "03:15:00";
+            try
+            {
+                TimeSpan convertedTimeSpan = TimeSpan.Parse(timeSpanStr);
+                TimeSpan addedTimeSpan = convertedTimeSpan.Add(new TimeSpan(2, 0, 0));
+                Console.WriteLine("Added TimeSpan: " + addedTimeSpan);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error occured: Invalid TimeSpan format");
+            }
+            Console.ReadKey();
+        }
+    }
+}
